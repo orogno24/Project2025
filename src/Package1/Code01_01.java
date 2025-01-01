5aae1487eb00f83d59a8734fa67117c81ca3f768
Ongoro <49369454+orogno24@users.noreply.github.com>
2025-01-01 22:52:56 +0900

    20250101

---
+++ src/Package1/Code01_01.java
@@ -0,0 +1,11 @@
+package Package1;
+
+public class Code01_01 {
+    public static void main(String[] args) {
+        newYear(2025);
+    }
+
+    public static void newYear(int num) {
+        System.out.println(num + ": Happy New Year!");
+    }
+}
